6f86a3665f3f97f6b8714de00aaa2e14e76bb7a6
Parq254 <juniorchris761@gmail.com>
2024-08-28 02:35:16 -0400

    initial commit

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -6,6 +6,7 @@ function ProductList() {
     const [showCart, setShowCart] = useState(false); 
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [addedToCart, setAddedToCart] = useState({});
+    const dispatch = dispatch();
     const handleAddToCart = (product) => {
         dispatch(addItem(product));
         setAddedToCart((prevState) => ({
